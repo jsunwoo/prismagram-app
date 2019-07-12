eb8e69286d60fa6e6739db47ebe4bce53ddf2755
jsunwoo <jinsung.sunwoo@gmail.com>
2019-07-12 10:42:58 +0900

    #10.1 Preloading Assets

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { AppLoading, Font, Asset } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
+  const [loaded, setLoaded] = useState(false);
+  const preLoad = async () => {
+    try {
+      await Font.loadAsync({
+        ...Ionicons.font
+      });
+      await Asset.loadAsync([require("./assets/icon.png")]);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoaded(true);
+  };
+
+  useEffect(() => {
+    preLoad();
+  }, []);
+
+  return loaded ? (
+    <View>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
+  ) : (
+    <AppLoading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -18,3 +18,12 @@ Screens:
 
 - 깃허브 세팅해준뒤 app.json 의 platforms 에서 web 은 지워줬다 (버그 많을까봐)
 - yarn add styled-components react-navigation apollo-boost graphql react-apollo-hooks
+
+
+
+# 10.1 Preloading Assets
+
+- @expo/vector-icons 을 추가해주었다
+- useEffect 도 Hook 인데 처음 보았고 아직 잘 모르겠다 (Nook 보고옴)
+- useEffect 로 preLoad 를 실행시켜 font 와 asset 들을 불러온 뒤 loaded state true 로 해줌
+
